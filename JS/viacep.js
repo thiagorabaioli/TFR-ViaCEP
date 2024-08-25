d95824fbd16ac9bb23580afe9667a08c4bd75dc2
thiagorabaioli <tfrabaioli@outlook.com>
2024-08-25 20:02:04 +0100

    Criando modalController e pageController. Fim

--- JS/viacep.js
+++ JS/viacep.js
@@ -1,7 +1,10 @@
 
 import * as formController from './controller/form-controller.js';
 import * as listController from './controller/list-controller.js';
-
+import * as modalController from './controller/modal-controller.js';
+import * as pageController from './controller/page-controller.js';
 
 formController.init();
 listController.init();
+modalController.init();
+pageController.init();
